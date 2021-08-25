9580d7cdfa93207bb44b28f0807f963e8a6657e4
jange <jange@DESKTOP-1PGUA82>
2021-08-25 15:00:40 +0200

    Added css to contact site, and worked on responsiv

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 //alert("script has been loaded!")
 
+//alert(getRandomNumber(1, 55));
+
+document.getElementById("btn--graderKlick").onclick = function getClickedRandomNumber()
+{
+    let min = Math.ceil(1);
+    let max = Math.floor(55);
+    let randomNumber = getRandomNumber(min, max);
+    document.getElementById("graderbox").innerHTML = `<h1>${randomNumber}</h1>`;
+};
+
+
+
+function getRandomNumber(min, max)
+{
+    return Math.floor(Math.random() * (max - min + 1) + min) + "°";
+}
